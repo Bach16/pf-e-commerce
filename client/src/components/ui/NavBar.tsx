43f0905fa337eaed4bc44af52feddeff765f5b0b
Judadimo22 <109996961+Judadimo22@users.noreply.github.com>
2023-03-29 17:06:31 -0500

    diseño NavBar

--- client/src/components/ui/NavBar.tsx
+++ client/src/components/ui/NavBar.tsx
@@ -1,16 +1,38 @@
-import { AppBar, Toolbar, Link } from '@mui/material';
+import { AppBar, Toolbar, Link, Box, IconButton } from '@mui/material';
 import NextLink from 'next/link'
 import Typography from '@mui/material/Typography';
+import { Menu, SearchOutlined, ShoppingCart, ShoppingCartOutlined } from '@mui/icons-material';
+import { Badge } from '@mui/material';
 
 export const NavBar = () => {
   return (
-    <AppBar color='inherit'>
-        <Toolbar>
-            <NextLink href='/' passHref>
-                <Link color='#ffffff'>
+    <AppBar >
+        <Toolbar >
+            <NextLink  href='/' passHref>
+                <Link sx={{pb:1}}>
                 <Typography variant='h6' color='black'>E-Commerce</Typography>
                 </Link>
             </NextLink>
+
+            <Box flex={1} />
+
+            <IconButton>
+                <SearchOutlined color='primary'/>
+            </IconButton>
+
+            <NextLink href='/cart' passHref>
+                <Link>
+                <IconButton>
+                    <Badge badgeContent={2} color='secondary'>
+                    <ShoppingCartOutlined color='primary'/>
+                    </Badge>
+                </IconButton>
+                </Link>
+            </NextLink>
+
+            <IconButton color='primary'>
+                <Menu/>
+            </IconButton>
         </Toolbar>
     </AppBar>
   )
