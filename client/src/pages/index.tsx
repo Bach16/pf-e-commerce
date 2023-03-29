825dc6acb360f0e10b0d7278fded8f6238e33815
Bach16 <gwyn56717@gmail.com>
2023-03-29 13:37:30 -0500

    add global styles

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,9 +1,7 @@
 import type {NextPage} from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
 import { ShopLayout } from '../components/layouts';
-import styles from '../styles/Home.module.css';
-import Typography from '@mui/material/Typography';
+import '../../styles/globals.css'
+
 
 const Home: NextPage = () => {
   return(
