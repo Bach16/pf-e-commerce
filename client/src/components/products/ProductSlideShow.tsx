c0df457b1179638811a82181064151295d4d78f4
Bach16 <gwyn56717@gmail.com>
2023-03-29 00:48:26 -0500

    Create ProductSlideShow component

---
+++ client/src/components/products/ProductSlideShow.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react"
+import { Slide } from "react-slideshow-image";
+import 'react-slideshow-image/dist/styles.css';
+import styles from "./ProductSlideShow.module.css"
+
+
+interface Props {
+  images: string[];
+}
+
+const ProductSlideShow: FC<Props> = ({images}) => {
+  return (
+    <Slide children
+    >
+{
+        images.map(image=>{
+          const url =`${image}`;
+          return(
+          <div className={styles['each-slide']} key={image}>
+            <div style={{
+                backgroundImage: `url(${url})`,
+                backgroundSize: "cover"
+            }} > 
+
+            </div>
+          </div>
+          )
+        })
+      }      
+    </Slide>
+  )
+}
+
+export default ProductSlideShow
