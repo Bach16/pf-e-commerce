86aeb9b27c733dd6c24e02e917f5b70f16de4517
Judadimo22 <109996961+Judadimo22@users.noreply.github.com>
2023-03-30 18:23:54 -0500

    Cambios NavBar
    Agrugué la SearchBar a la NavBar

--- client/src/components/ui/NavBar.tsx
+++ client/src/components/ui/NavBar.tsx
@@ -1,14 +1,30 @@
-import { AppBar, Toolbar, Link, Box, IconButton } from '@mui/material';
+import { AppBar, Toolbar, Link, Box, IconButton, InputAdornment, Input } from '@mui/material';
 import NextLink from 'next/link'
 import Typography from '@mui/material/Typography';
 import { Menu, SearchOutlined, ShoppingCart, ShoppingCartOutlined } from '@mui/icons-material';
 import { Badge } from '@mui/material';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { UiContext } from '../../context'
+import { useRouter } from 'next/router';
 
 export const NavBar = () => {
 
-    const {toggleSideMenu} = useContext(UiContext)
+    const {toggleSideMenu} = useContext(UiContext);
+
+    const router = useRouter();
+
+
+
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const onSearchTerm = () => {
+        if(searchTerm.trim().length === 0) return;
+        navigateTo(`/search/${searchTerm}`)
+    }
+
+    const navigateTo = (url: string) => {
+        router.push(url)
+    }
   return (
     <AppBar >
         <Toolbar >
@@ -20,9 +36,22 @@ export const NavBar = () => {
 
             <Box flex={1} />
 
-            <IconButton>
-                <SearchOutlined color='primary'/>
-            </IconButton>
+            <Input
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                        onKeyPress={(e) => e.key === 'Enter' ? onSearchTerm() : null}
+                        type='text'
+                        placeholder="Buscar..."
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                aria-label="toggle password visibility"
+                                >
+                                 <SearchOutlined />
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                    />
 
             <NextLink href='/cart' passHref>
                 <Link>
