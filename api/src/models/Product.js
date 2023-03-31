e33b1d014b78c4fff5dfee19fc29e7345cc5ba0d
Delamm <eldygonzalez.09@hotmail.com>
2023-03-31 10:08:39 -0300

    Corrección de controllers

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,40 +1,43 @@
-import mongoose, {Schema, model, Model} from 'mongoose';
+import mongoose, { Schema, model, Model } from "mongoose";
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 
-const productSchema= new Schema({
-    trademark:{type: String, required: true},
-    image:[{type: String, required: true}],
-    stock:{type: Number, required: true, default:0},
-    price:{type: Number, required: true, default:0},
-    size:[{
+const productSchema = new Schema(
+  {
+    trademark: { type: String, required: true },
+    image: [{ type: String, required: true }],
+    stock: { type: Number, required: true, default: 0 },
+    price: { type: Number, required: true, default: 0 },
+    size: [
+      {
         type: String,
         enum: {
-            values: ['XS','S','M','L','XL','XXL','XXXL'],
-            message: '{VALUE} no es un tamaño valido'
-        }
-    }],
-    description:{type: String, required: true},
-    tags:[{type: String}],
+          values: ["XS", "S", "M", "L", "XL", "XXL", "XXXL"],
+          message: "{VALUE} no es un tamaño valido",
+        },
+      },
+    ],
+    description: { type: String, required: true },
+    // tags:[{type: String}],
     type: {
-        type: String,
-        enum: {
-            values: ['shirts','pants','hoodies','hats'],
-            message: '{VALUE} no es un tipo valido'
-        }
+      type: String,
+      enum: {
+        values: ["shirts", "pants", "hoodies", "hats"],
+        message: "{VALUE} no es un tipo valido",
+      },
     },
-    categorie:{
-        type: String,
-        enum: {
-            values: ['men','women','kid','unisex'],
-            message: '{VALUE} no es una categoria valida'
-        }
-    }
-},{timestamps: true})
+    categorie: {
+      type: String,
+      enum: {
+        values: ["men", "women", "kid", "unisex"],
+        message: "{VALUE} no es una categoria valida",
+      },
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = model('Product', productSchema);
-
-
+module.exports = model("Product", productSchema);
 
 // module.exports = (sequelize) => {
 //     sequelize.define(
