2ebc90f081fcace3c48c75ae96ab36a37785f0a9
Delamm <eldygonzalez.09@hotmail.com>
2023-04-01 16:18:47 -0300

    Actualización de Consumer a mongoose

--- api/src/models/Consumer.js
+++ api/src/models/Consumer.js
@@ -1,43 +1,74 @@
-const { DataTypes } = require("sequelize");
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+const mongoose = require("mongoose");
 
-module.exports = (sequelize) => {
-    sequelize.define(
-        "consumer",
-        {
-            id: {
-                type: DataTypes.UUID,
-                defaultValue: DataTypes.UUIDV4,
-                allowNull: false,
-                primaryKey: true,
-              },
-            email: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                validate: {
-                    isEmail: true
-                },
-                unique: true
-            },
-            addres: {
-                type: DataTypes.STRING,
-                allowNull: false,
+const consumerSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    country: {
+      type: String,
+      ref: "Location",
+    },
+    addres: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      unique: true,
+      minLength: 9,
+    },
+  },
+  { timestamps: true }
+);
 
-            },
-            phone: {
-                type: DataTypes.INTEGER,
-                allowNull: false
-            },
-            country: {
-                type: DataTypes.STRING,
-                allowNull: false
-            },
-            name: {
-                type: DataTypes.STRING,
-                allowNull: false,
-              },
+module.exports = mongoose.model("Consumer", consumerSchema);
+// const { DataTypes } = require("sequelize");
+// // Exportamos una funcion que define el modelo
+// // Luego le injectamos la conexion a sequelize.
 
-        },{ timestamps: false }
-        )
-}
+// module.exports = (sequelize) => {
+//     sequelize.define(
+//         "consumer",
+//         {
+//             id: {
+//                 type: DataTypes.UUID,
+//                 defaultValue: DataTypes.UUIDV4,
+//                 allowNull: false,
+//                 primaryKey: true,
+//               },
+//             email: {
+//                 type: DataTypes.STRING,
+//                 allowNull: false,
+//                 validate: {
+//                     isEmail: true
+//                 },
+//                 unique: true
+//             },
+//             addres: {
+//                 type: DataTypes.STRING,
+//                 allowNull: false,
+
+//             },
+//             phone: {
+//                 type: DataTypes.INTEGER,
+//                 allowNull: false
+//             },
+//             country: {
+//                 type: DataTypes.STRING,
+//                 allowNull: false
+//             },
+//             name: {
+//                 type: DataTypes.STRING,
+//                 allowNull: false,
+//               },
+
+//         },{ timestamps: false }
+//         )
+// }
