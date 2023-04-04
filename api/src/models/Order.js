736e79a114d4da69d7331547cc6aa5e7d1e5fb21
Delamm <eldygonzalez.09@hotmail.com>
2023-04-04 05:57:09 -0300

    Empezando integración de MP

--- api/src/models/Order.js
+++ api/src/models/Order.js
@@ -4,13 +4,28 @@ const orderSchema = mongoose.Schema({
   id_pay: {
     type: String,
   },
-  products: {
+  items: {
+    type: Array,
+  },
+  date_approved: {
+    type: String,
+  },
+  operation_type: {
+    type: String,
+  },
+  order: {
     type: Array,
   },
   payer: {
     type: Array,
   },
   status: {
+    type: String,
+  },
+  status_detail: {
+    type: String,
+  },
+  statusProduct: {
     type: String,
     enum: ["delivered", "undelivered"],
     default: "undelivered",
---
+++ api/src/mp/mercadopago.js
@@ -0,0 +1,8 @@
+// SDK de Mercado Pago
+const mercadopago = require("mercadopago");
+// Agrega credenciales
+mercadopago.configure({
+  access_token: process.env.MERCADOPAGO_KEY,
+});
+
+module.exports = { mercadopago };
