11bd46b855d5c6a30ae16ce91bfe162684624919
Delamm <eldygonzalez.09@hotmail.com>
2023-04-04 05:13:23 -0300

    Creación de Rutas de review

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,9 +4,11 @@ const router = Router();
 const productRouter = require("./Product");
 const userRoutes = require("./Users");
 const contactRoutes = require("./ContactUs");
+const reviewRoutes = require("./Review");
 
 router.use("/cloth", productRouter);
 router.use("/users", userRoutes);
 router.use("/contact", contactRoutes);
+router.use("/review", reviewRoutes);
 
 module.exports = router;
