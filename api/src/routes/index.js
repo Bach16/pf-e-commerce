bec21c9563f92be5d548f8b7b6e387afd75276b2
Delamm <eldygonzalez.09@hotmail.com>
2023-04-04 04:07:33 -0300

    Creación de rutas para contactenos

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,7 +2,11 @@ const { Router } = require("express");
 const router = Router();
 
 const productRouter = require("./Product");
+const userRoutes = require("./Users");
+const contactRoutes = require("./ContactUs");
 
 router.use("/cloth", productRouter);
+router.use("/users", userRoutes);
+router.use("/contact", contactRoutes);
 
 module.exports = router;
